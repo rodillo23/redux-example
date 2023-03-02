a6135e7c60bd43dbf1efb39b4132823c0d0e1e87
Rodolfo Figueroa <ficr23@hotmail.com>
2023-03-02 15:44:00 -0600

    PokemonApp: se agrego Componente y se creo el slice de redux

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const PokemonApp = () => {
+  return (
+    <>
+        <h1>Pokemon App</h1>
+        <hr />
+
+        <ul>
+            <li>Hola</li>
+            <li>Hola</li>
+            <li>Hola</li>
+        </ul> 
+    </>
+  )
+}
